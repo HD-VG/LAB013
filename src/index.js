a237a82a17997f978dbbf2e8d2d61f3478d1f277
HD-VG <90581333+HD-VG@users.noreply.github.com>
2024-06-11 08:58:36 -0400

    puerto 3000

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import medicRouter from './routers/MedicRouter.js'
 const app = express()
 app.use(express.json())
 app.use('/routers',medicRouter)
-app.listen(5000, () => {
-    console.log('Server on port : ', 5000);
+app.listen(3000, () => {
+    console.log('Server on port : ', 3000);
 });
